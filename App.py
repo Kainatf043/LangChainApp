23d80f745cf0d9bcf1d6603707bb38ba01a7a715
Kainatf043 <120456576+Kainatf043@users.noreply.github.com>
2024-05-27 02:40:16 -0700

    Add files via upload

--- App.py
+++ App.py
@@ -6,7 +6,8 @@ def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
     st.info(llm(input_text))
 with st.form('my_form'):
-    text = st.text_area('Enter text:')
+    text = st.text_area('Enter text:'
+                        )
     submitted = st.form_submit_button('Submit')
     if not openai_api_key.startswith('sk-'):
         st.warning('Please enter your OpenAI API key', icon='⚠️')
